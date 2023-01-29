9f796119764252b946d671d754aa76fbeb8b007b
K.Venketesh Dora <kvenketeshdora@gmail.com>
2023-01-29 19:23:15 +0530

    Prime

--- Prime.java
+++ Prime.java
@@ -1,5 +1,22 @@
+import java.util.*;
+
 public class Prime {
     public static void main(String args[]){
-        
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int temp = 0;
+        for(int i=2; i<=n-1; i++){
+            if(n % i == 0){
+                temp++;
+                break;
+            }
+        }
+
+        if(temp == 0){
+            System.out.println("Prime");
+        }else{
+            System.out.println("Not Prime");
+        }
+        sc.close();
     }
 }
