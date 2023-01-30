6cb9e3924bfaf2f825f0a17e71d6228f86533837
K.Venketesh Dora <kvenketeshdora@gmail.com>
2023-01-30 14:19:14 +0530

    Type casting

---
+++ Casting.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Casting{
+    public static void main(String args[]){
+        //Widening casting
+        // int a = 5;
+        // long b = a;
+        // System.out.println(b);
+
+        //Narrowing casting
+        long a = 80;
+        int b = (int)a;
+        System.out.println(b);
+    }
+}
