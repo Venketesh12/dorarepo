385915c82e5fc0719c0d6f6e37c2c0201a922a8e
K.Venketesh Dora <kvenketeshdora@gmail.com>
2023-01-30 14:25:07 +0530

    Typecasting

--- Casting.java
+++ Casting.java
@@ -1,4 +1,3 @@
-import java.util.*;
 
 public class Casting{
     public static void main(String args[]){
@@ -8,7 +7,7 @@ public class Casting{
         // System.out.println(b);
 
         //Narrowing casting
-        long a = 80;
+        double a = 9.78;
         int b = (int)a;
         System.out.println(b);
     }
