9de71f5c3bf554721afa113e3eefaeb07e513e63
K.Venketesh Dora <kvenketeshdora@gmail.com>
2023-02-06 19:39:00 +0530

    *

--- Array.java
+++ Array.java
@@ -1,12 +1,14 @@
 public class Array {
-    public static void update(int marks[]){
+    public static void update(int marks[],int non){
         for(int i=0; i<marks.length; i++){
             marks[i] = marks[i] + 1;
         }
     }
     public static void main(String args[]){
         int marks[] = {20,10,30};
-        update(marks);
+        int non = 5;
+        update(marks,non);
+        System.out.println(non);
         for(int i=0; i<marks.length; i++){
             System.out.print(marks[i] + " ");
         }
