8db4d326425d35a22ba36639e879a00b4a8e0a89
K.Venketesh Dora <kvenketeshdora@gmail.com>
2023-07-01 19:31:28 +0530

    .

--- Practice2.java
+++ Practice2.java
@@ -1,8 +1,6 @@
 public class Practice2 {
     public static void main(String args[]){
-        int r = 3;
-        double area = 2 * Math.PI * r;
-        System.out.println(area);
+        
     }
 }
 
